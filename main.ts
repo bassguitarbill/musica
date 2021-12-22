2af8e15ceb68501fc221e8f40031d31ef0b4f5c7
Bill Rossi <bassguitarbill@gmail.com>
2021-12-22 06:50:33 -0500

    Save and load chord weights

--- main.ts
+++ main.ts
@@ -28,6 +28,52 @@ showKeyPickerButton.addEventListener('click', showKeyPicker);
 const sheetMusicCanvas = document.getElementById('SheetMusic')! as HTMLCanvasElement;
 const sheetMusic = new SheetMusic(song, sheetMusicCanvas);
 
+document.querySelectorAll('input[type=range]').forEach(e => {
+  e.addEventListener('change', _ => {
+    saveChordWeights();
+  });
+});
+
+function saveChordWeights() {
+  const weights:{ [k:string]: string} = {};
+  document.querySelectorAll('input[type=range]').forEach(e => {
+    weights[e.id] = (e as HTMLInputElement).value;
+  });
+  window.localStorage.setItem('chordWeights', JSON.stringify(weights));
+  console.log('saved');
+}
+
+let majorChordWeights = [8, 5, 3, 5, 7, 6, 2];
+let minorChordWeights = [8, 5, 3, 5, 7, 6, 2];
+
+const defaultChordWeights = {
+  'major-I': 8, 'major-ii': 5, 'major-iii': 3, 'major-IV': 5,
+  'major-V': 7, 'major-vi': 6, 'major-vii': 2,
+  'minor-i': 8, 'minor-ii': 5, 'minor-III': 3, 'minor-iv': 5,
+  'minor-v': 8, 'minor-VI': 8, 'minor-VII': 8, 
+}
+
+function loadChordWeights() {
+  const chordWeights = window.localStorage.getItem('chordWeights');
+  let cw:any = defaultChordWeights;
+  if (chordWeights) {
+    cw = JSON.parse(chordWeights);
+  }
+  return cw;
+}
+
+function applyChordWeights() {
+  const cw = loadChordWeights();
+  majorChordWeights = [ cw['major-I'], cw['major-ii'], cw['major-iii'], cw['major-IV'],
+                        cw['major-V'], cw['major-vi'], cw['major-vii']];
+  minorChordWeights = [ cw['minor-i'], cw['minor-ii'], cw['minor-III'], cw['minor-iv'],
+                        cw['minor-v'], cw['minor-VI'], cw['minor-VII']];
+  document.querySelectorAll('input[type=range]').forEach(e => {
+    (e as HTMLInputElement).value = cw[e.id];
+  });
+}
+applyChordWeights();
+
 function chooseOneFromArray(arr: any[]) {
   const index = Math.floor(Math.random() * arr.length);
   return arr[index];
@@ -118,7 +164,10 @@ function toggleChordWeights() {
 function chooseChords(numberOfChords: number) {
   song.chords = [];
   for (let i=0; i<numberOfChords; i++) {
-    song.chords.push(chooseOneFromArrayWeighted(song.key!.chords, song.key!.chordWeights));
+    const isMinor = song.key!.isMinor;
+    const chordWeights = isMinor ? minorChordWeights : majorChordWeights;
+
+    song.chords.push(chooseOneFromArrayWeighted(song.key!.chords, chordWeights));
   }
   displayChords();
 }
